1b680aed7c1a543adddbd6e07988699f1be75f2c
Sebastián <jimenezsebastian994@gmail.com>
2025-05-14 13:52:21 -0600

    Exceptions

---
+++ src/StudentNameException.java
@@ -0,0 +1,5 @@
+public class StudentNameException extends Exception {
+    public StudentNameException(String message) {
+        super(message);
+    }
+}
---
+++ src/StudentUnderAgeException.java
@@ -0,0 +1,8 @@
+// Custom exception class for underage students
+public class StudentUnderAgeException extends Exception {
+    public StudentUnderAgeException(String message) {
+        super(message);
+    }
+}
+
+
